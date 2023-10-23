916224452321ccd5e5617db951caa0ba1e35259a
Kadda Aoues <houes_k@yahoo.fr>
2023-10-23 11:12:25 +0300

    new concepts

--- Cpp_Learning/Concepts_4.cpp
+++ Cpp_Learning/Concepts_4.cpp
@@ -32,6 +32,15 @@ void func(const T& x) {
 	print_ << "is array " << end_;
 }
 
+
+// this take array
+template<template<typename,size_t> class Array,typename T, size_t N>
+requires std::is_same_v<Array<T,N>,std::array<T,N>>
+void func(const Array<T,N>& x) {
+	std::cout << "is an Array of size : " << x.size() << end_;
+	std::cout << "type is             : " << typeid(T).name() << end_;
+}
+
 int main()
 {
 	func<33>();
@@ -44,11 +53,11 @@ int main()
 	std::array<int, 3> arr;
 
 	int a[5]{};
-	func(a);
+	func(a);  // func<int[5]>(const int (&x)[5])
 
-	// func({ 3,6,7 }); have no
+	// func({ 3,6,7 }); //have no
 
-	// func(arr); 
+	 func(arr); 
 
 
 	std::cin.get();
