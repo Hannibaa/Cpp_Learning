0ed775d65d02c60eb887424dde6411298eee0a94
Kadda Aoues <houes_k@yahoo.fr>
2023-10-26 09:47:35 +0300

    exemple of panning from cppcon;

--- Cpp_Learning/floating_increment.cpp
+++ Cpp_Learning/floating_increment.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "c:\Users\Acer\source\MyLib/Console_Library/escape_code.h"
-#include "c:\Users\Acer\source\MyLib\math_functions.h"
+#include "MyLib/Console_Library/escape_code.h"
+#include "MyLib\function_utility.h"
 
 
 using namespace std;
@@ -15,13 +15,14 @@ int main()
 
 	print_ << "integer conversion bit to bit : " << std::bit_cast<int64_t>(f) << end_;
 	print_ << "double f         : " << f << end_;
-	print_ << "increment by 1   : " << Math::float_inc(f) << end_;
-	print_ << "increment by 100 : " << Math::float_inc(f,100) << end_;
+	print_ << "increment by 1   : " << Function::float_inc(f) << end_;
+	print_ << "increment by 100 : " << Function::float_inc(f,100) << end_;
 	print_ << "float of int (1)   " << std::bit_cast<float>(1) << end_;
 	print_ << "float of int (100) " << std::bit_cast<float>(100) << end_;
 	wait_;
 	for (int i = 0; i < 10000; ++i) {
-		print_ << "increment by 1 : " << Math::float_inc(f,10000000) << end_;
+		auto ff = Function::float_inc(f, 1000);
+		print_ << "increment by 1 : " << ff << end_;
 	}
 
 	cin.get();
