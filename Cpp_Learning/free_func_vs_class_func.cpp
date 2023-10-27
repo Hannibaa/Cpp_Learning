28742396173b8b03d1dbfd0e48b94b95abbbbaed
Kadda Aoues <houes_k@yahoo.fr>
2023-10-28 01:23:48 +0300

    overloading functions

--- Cpp_Learning/free_func_vs_class_func.cpp
+++ Cpp_Learning/free_func_vs_class_func.cpp
@@ -11,14 +11,12 @@
 
 */
 
+// f functions
 
 int f(float x, std::string str = "") {
 	std::cout << "call f(float,string = 0)->int \n";
 	return 100;
 }
-
-
-
 
 float f(int x, float y) {
 	std::cout << "call f(int,flaot)->float \n";
@@ -31,6 +29,7 @@ double f(int x) {
 	return double(x);
 }
 
+// function under class
 
 class Func_Test {
 
@@ -58,6 +57,31 @@ public:
 
 };
 
+// g functions 
+// using concept
+
+float g(const auto& x)
+requires std::is_same_v<decltype(x), int>
+{
+	return static_cast<float>(x);
+}
+
+float g(const auto& x)
+	requires (x > 10)
+{
+	std::cout << " x > 10 \n";
+	
+	return float(x);
+}
+
+float g(const auto& x)
+	requires (x < 0)
+{
+	std::cout << " x < 0 \n";
+	return float(x)
+}
+
+
 
 int main() {
 
@@ -73,6 +97,11 @@ int main() {
 	std::cout << f1.f(45);
 	std::cout << f1.f(10, 2.3f) << '\n';
 	std::cout << f1.f(2.3f, "here") << '\n';
+
+	// test for g with concept
+	std::cout << "test functions g : \n";
+	g(10);
+	
 
 	std::cin.get();
 	return 0;
