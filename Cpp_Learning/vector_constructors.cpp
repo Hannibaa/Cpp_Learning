1afc43981f62ddebac148ce7819256172f9fd782
Kadda Aoues <houes_k@yahoo.fr>
2023-11-05 00:09:54 +0300

    add more example on constructors

--- Cpp_Learning/vector_constructors.cpp
+++ Cpp_Learning/vector_constructors.cpp
@@ -11,13 +11,26 @@
 
 int main()
 {
+	// constructor initial_list
 	std::vector vInt{ 1,3,4,5,6,7,5 };
 
-	// position + size 
+	// constructor 
 	std::vector vInt1(vInt.begin()+1, vInt.begin()+4);
+
+	// constructor difference between () and {};
+	std::vector<int>  vInt2(10, 200);
+	std::vector<int> vInt3{ 10,200 };
+	std::vector<int> vInt4(10);
+	std::vector<int> vInt5{ 10 };
+
+
 
 	vu::print_container(vInt);
 	vu::print_container(vInt1);
+	vu::print_container(vInt2);
+	vu::print_container(vInt3);
+	vu::print_container(vInt4);
+	vu::print_container(vInt5);
 
 
 	std::cin.get();
