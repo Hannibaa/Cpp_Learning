0671a938949dcf3a9560c0ab34f64d93d75ba83b
Kadda Aoues <houes_k@yahoo.fr>
2023-11-13 22:36:45 +0300

    add comparaison to erase member function

--- Cpp_Learning/std_copy.cpp
+++ Cpp_Learning/std_copy.cpp
@@ -4,6 +4,9 @@
 #include "MyLib/Console_Library/escape_code.h"
 #include <MyLib/vector_utility1.h>
 
+/* 
+    this exemple comparing two methode of erasing element from vector.
+*/
 
 template<typename T>
 std::vector<T> erase_from_to(const std::vector<T>& vec, size_t pos, size_t size) {
@@ -25,6 +28,8 @@ std::vector<T> erase_from_to(const std::vector<T>& vec, size_t pos, size_t size)
 int main()
 {
 	std::vector<int>  v{ 0,1,2,3,4,5,6,7,8,9 };
+	auto w = v;
+
 	print_ << "size of v : " << v.size() << end_;
 	vu::print_container(v, "vector v : ");
 	newline_;
@@ -34,11 +39,16 @@ int main()
 
 	v = vu::erase_from_to(v, pos, size);
 
+	w.erase(w.begin() + pos, w.begin() + pos + size);
+
 	char text[100]{};
 
 	sprintf_s(text, "v after erasing %d element from position %d :", size, pos);
 
+
 	vu::print_container(v, text);
+	text[0] = 'w'; newline_;
+	vu::print_container(w, text);
 
 	std::cin.get();
 	return 0;
