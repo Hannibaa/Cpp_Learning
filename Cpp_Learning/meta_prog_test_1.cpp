5a50470da042d1081d63a83d76ea6ac0d862a950
Kadda Aoues <houes_k@yahoo.fr>
2023-11-16 13:43:03 +0300

    maetaprogramming test 1

--- Cpp_Learning/meta_prog_test_1.cpp
+++ Cpp_Learning/meta_prog_test_1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "MyLib/Console_Library/escape_code.h"
 #include <MyLib/function_utility.h>
+#include <MyLib/my_metaprog.h>
 
 
 template<typename T>
 void print_sizeof() {
-	print_ << "size of " << typeid(T).name() << " is : " << Fn::max_value<T>() << end_;
+	print_ << "size of " << typeid(T).name() << " is : " << Fn::max_v<T>() << end_;
 }
 
 int main()
@@ -14,11 +15,17 @@ int main()
 	print_sizeof<T>();
 	print_sizeof<long long>();
 	print_sizeof<size_t>();
-
+	print_sizeof<std::string>();
 	print_ << "max of int " << Fn::max_of<int> << end_;
 	print_ << "max of char " << Fn::max_of<char> << end_;
+	print_ << "max of size_t " << Fn::min_of<long> << end_;
+	print_ << "max of string class " << Fn::max_of<std::string> << end_;
 
-	print_ << "max of size_t " << Fn::max_of<size_t> << end_;
+	wait_; cls();
+
+	meta::first_arg_t<int, char, float> x{};
+	meta::get_nth_type_t<2, int, char, float> y{};
+
 
 	std::cin.get();
 	return 0;
