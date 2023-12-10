d6215af641506536847a5019548480faaeaa5ccd
Kadda Aoues <houes_k@yahoo.fr>
2023-12-11 02:30:26 +0300

    table macro test

--- Cpp_Learning/Table_macro_structure_console.cpp
+++ Cpp_Learning/Table_macro_structure_console.cpp
@@ -3,35 +3,20 @@
 
 #define printm_(x,y)   print_ << MOVETO(x,y)
 
-std::string add_line(int x, int y, table::STYLE s, table::STYLE s_last) {
-	std::stringstream ss;
 
-	ss << UNDERLINE << _cell(std::to_string(x), s, right);
-	ss << UNDERLINE << _cell(std::to_string(y), s_last, left);
-	ss << end_;
-
-	return ss.str();
-}
 
 int main()
 {
-	const char solid_left     = '\xdd';
-	const char solid_right    = '\xde';
-	const char solid_complete = '\xdb';
-	const char solid_up       = '\xdf';
-	const char solid_down     = '\xdc';
-	const char solid_center   = '\xfe';
 
+	// declare style of table
 	table::STYLE s_title = table::make_style(color::Fuchsia, color::Grey15, 41, 
 		table_vline, table_vline);
 	table::STYLE s = table::make_style(color::Aqua, color::Grey23, 20, table_vline, 0);
-	table::STYLE s_last = table::make_style(color::Aqua, color::Grey23, 20, table_vline, table_vline);
+	table::STYLE s_last = table::make_style(color::Aqua, color::Grey23, 20, 
+		table_vline, table_vline);
 	
 
 	
-
-	std::string name1 = "editor";
-	std::string name2 = "Names";
 
 	//print_ << Cell_text(name1, s, left) << end_;
 	print_ << UNDERLINE << _cell("            value of function ", s_title, left) << end_;
@@ -43,12 +28,6 @@ int main()
 		print_ << table::add_line_table(i, s, 3*i , s_last);
 	}
 
-	print_ << SAVECURSORPOSITION;
-	printm_(70, 1) << "New value x : "; int ii;
-	std::cin >> ii;
-	printm_(70, 2) << "New value y : "; int jj;
-	std::cin >> jj;
-	print_ << RESTORCUR_SAVED_POS;
 
 	print_ << table::add_line_table(ii, s, jj, s_last);
 
