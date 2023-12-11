1cf1107fc6789030b6340fbb049951d76e5d61e9
Kadda Aoues <houes_k@yahoo.fr>
2023-12-11 10:29:01 +0300

    test new functionality

--- Cpp_Learning/table_test.cpp
+++ Cpp_Learning/table_test.cpp
@@ -7,8 +7,15 @@ int main()
 {
 	table::Table<int, float>  table(10, 10, "function plot");
 
+	
+
+	table.set_style(table::make_style(color::Yellow, color::Grey23, 15,
+		table_vline, table_vline));
+	
+
 	table.add_line(21, 4.55f);
 	table.add_line(40, 5.66f);
+	table.set_titlesXY("incremental", "ROP ins");
 
 	table.draw();
 
