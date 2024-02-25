60ea91e094202fc3c82a2d58738242ebb397ffa8
Kadda Aoues <houes_k@yahoo.fr>
2024-02-25 04:16:29 +0300

    test of erase member function in vector, iterators, making function with reference.

---
+++ Cpp_Learning/Algorithm/std_vector_erase.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+
+#include <MyLib/random_generator.h>
+
+#define end_   '\n'
+#define wait_   std::cin.get()
+
+struct Particule {
+	int  _id;
+	float x;
+	float y;
+
+	Particule() :_id{}, x{}, y{} {}
+
+
+	Particule(float _x , float _y , int id = 0) 
+		: _id{id}, x{_x}, y{_y}
+	{}
+
+};
+
+template<typename P>
+void create_100_particules(std::vector<P>&  vec) {
+	vec.reserve(110);
+	rng::fRG<float> frand;
+	for (int i = 0; i < 100; ++i)
+	{
+		vec.push_back(P{frand(0.f,100.f), frand(0.f,100.f) , i });
+	}
+}
+
+template<typename V>
+void print_vector(const V& v, int k) {
+
+	for (const auto& e : v) {
+		std::cout << "[ " << e._id << " | " << e.x << " , " << e.y << "]\n";
+		if (!--k) break;
+	}
+
+}
+
+template<typename T>
+struct Box {
+	T x;
+	T y;
+	T w;
+	T h;
+
+	bool contain(T a, T b) const {
+		return  a >= x && a <= x + w && b >= y && b <= y + h;
+	}
+};
+
+int numbers_particule_in_box(std::vector<Particule>& vecp, Box<float>& box) {
+	int count{};
+	for (const auto& p : vecp) {
+		if (box.contain(p.x, p.y)) ++count;
+	}
+
+	return count;
+}
+
+
+int main() {
+
+	std::vector<Particule> vec;
+
+	create_100_particules(vec);
+
+	std::cout << "capacity of vector : " << vec.capacity() << end_;
+	std::cout << "size of vector : " << vec.size() << end_;
+
+	print_vector(vec, 15);
+
+	// etablish logic to delete some value that that particule contain in some region in 0 and 100
+	// for exemple box(x,y,w,h)
+
+	Box<float> box{.x = 0.f, .y = 0.f, .w = 100.f, .h = 10.f};
+
+
+	int times_0{};
+	while (numbers_particule_in_box(vec, box) == 0) {
+		++times_0;
+		vec.clear();
+		create_100_particules(vec);
+		std::cout << "times : " << times_0 << end_;
+		std::cin.get();
+	}
+
+	std::cout << "number of times get 0 : " << times_0 << end_;
+	std::cout << "number of point in box is " << numbers_particule_in_box(vec, box) << end_;
+
+	// we goint to remove this point :
+	wait_;
+	for (auto it = vec.begin(); it != vec.end(); ) {
+		if (box.contain(it->x, it->y)) {
+			std::cout << "point id : " << it->_id << end_;
+			it = vec.erase(it);
+			// it return iterator after
+			std::cout << "point id : " << it->_id << end_;
+		}
+		else ++it;
+	}
+
+	std::cout << "new size of vector is : " << vec.size() << end_;
+
+
+	std::cout << "End of program...\n";
+	std::cin.get();
+	return 0;
+}
