487c047054aef4a97ef494f6ec7abad3eaaab8b2
Kadda Aoues <houes_k@yahoo.fr>
2024-04-04 20:34:17 +0300

    operator on standard vector

---
+++ Cpp_Learning/operator/operators.hpp
@@ -0,0 +1,19 @@
+/*
+          This header file will include all defined operator for 
+          1. Vector<T>
+          2. String(and basic_string)
+          3. Classes Generalization
+*/
+
+#pragma once
+
+
+inline namespace Operator {
+
+
+
+
+
+}
+
+namespace opr = Operator;
